285b302be0c7f14ebadbabe119aed80e7fd1a25c
Ru-Ri-z <69484119+Ru-Ri-z@users.noreply.github.com>
2022-02-25 22:06:14 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.container', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+        });
+
+    sr.reveal('.coverContainer', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.principalText', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.container', {
+        rotate: {
+            x: 5,
+            y: 150,
+    }
+    })
